83dadb3f36d370cc5995a250874f8cdadaae7e4e
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-02-28 17:30:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ function whisper(string){
 }
 
 function logShout(string){
-  console.log(string.toUpperCase);
+  console.log(string.toUpperCase());
 }
 
 function logWhisper(string){
-  console.log(string.toLowerCase);
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string){
-  if (string===string.toLowerCase) {
+  if (string===string.toLowerCase()) {
     return `I can't hear you!`;
   }
-  if (string===string.toUpperCase) {
+  if (string===string.toUpperCase()) {
     return `YES INDEED!`;
   }
   if (string===`I love you, Grandma.`) {
