a468ae986bbf5f7c5992f41a56e4d8c34e06c7be
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-02-28 17:29:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,7 @@ function sayHiToGrandma(string){
   if (string===string.toUpperCase) {
     return `YES INDEED!`;
   }
-  if (string===`)
+  if (string===`I love you, Grandma.`) {
+    return `
+  }
 }
