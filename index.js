db6fd574643912f3aa977774bab513e09093385e
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-02-28 17:26:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,13 @@ function whisper(string){
 }
 
 function logShout(string){
+  console.log(string.toUpperCase);
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase);
+}
+
+function sayHiToGrandma(string){
   
 }
